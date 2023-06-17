f0c1ae3bb39a0c216f11336b5619a2a90546aebf
Remilia517 <wer77788901@gmail.com>
2023-06-17 23:51:02 +0800

    modified:   homework/05/text.js 	modified:   homework/05/enumerate.js 	modified:   homework/05/prime.js 	homework/06/

--- homework/05/text.js
+++ homework/05/text.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <= 9; i++) {
+for (let i = 1;i <= 9;i++) {
     let row = "";
-    for (let j = 1; j <= 9; j++) {
+    for (let j = 1;j <= 9;j++) {
       row += `${i} * ${j} = ${i * j}\t`;
     }
     console.log(row);
